038b49846e99da52c4c95bd6e430e523af960aae
Jeferson Brandao de Novaes <jefersonbrandaodenovaes@gmail.com>
2024-11-06 15:30:17 -0300

    Add files via upload
    Programming the layout in React Native from VS Code

---
+++ _layout.tsx
@@ -0,0 +1,68 @@
+
+import React,{ useState  } from "react";
+import { Button, Text, View, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+export default function App(){
+  const [nome, setNome] = useState(''); //estado para armazenar os dados do formulário
+  const [idade, setIdade] = useState('');
+  const [resultado, setResultado] = useState('');  
+  const mostrarResultado =()=>{setResultado("Nome: ${nome}, Idade: ${idade}");};
+
+  //inserindo o texto na tela
+  return(
+    <View>
+      <Text>Formulário dos Crias</Text>
+
+      <TextInput
+          placeholder="Nome"
+          value={nome}
+          onChangeText={setNome} //jogando o valor Nome vai ser carregado no formulario
+      />
+
+      <TextInput
+          placeholder="Idade"
+          value={idade}
+          onChangeText={setIdade} //jogando o valor Nome vai ser carregado no formulario
+      />
+
+      <Button title="Mostrar Resultado" onPress= {mostrarResultado} ></Button>
+
+
+      {resultado?(
+        <Text>{resultado}</Text>):null} 
+    </View>
+  )  
+}
+
+const style = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor:'#fff',
+  },
+
+  tittle:{
+    fontSize:24,
+    fontWeight:'bold',
+    textAlign:'center',
+    marginBottom:20,
+  },
+
+  input:{
+    height:40,
+    borderColor:'#ccc',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+
+  resultado:{
+    marginTop: 20,
+    fontSize: 18,
+    color:'#333',
+    textAlign:'center',
+  }
+})
