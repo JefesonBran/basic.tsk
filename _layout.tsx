6e7d30a8b50fdfc757f956679c43080247663c22
Jeferson Brandao de Novaes <jefersonbrandaodenovaes@gmail.com>
2024-11-09 23:58:17 -0300

    Add files via upload

--- _layout.tsx
+++ _layout.tsx
@@ -1,32 +1,66 @@
-
 import React,{ useState  } from "react";
 import { Button, Text, View, StyleSheet } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 export default function App(){
   const [nome, setNome] = useState(''); //estado para armazenar os dados do formulário
+  const [cargo, setCargo] = useState('');
   const [idade, setIdade] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [email, setEmail] = useState('');
+  const [da, setDA] = useState('');
   const [resultado, setResultado] = useState('');  
-  const mostrarResultado =()=>{setResultado(`Nome: ${nome}, Idade: ${idade}`);};
+  const mostrarResultado =()=>{setResultado(`
+    Nome:${nome} -
+    Cargo:${cargo} -
+    Idade:${idade} -
+    Telefone:${telefone} -
+    Email:${email} - 
+    Data De Admissão:${da}
+    `);};
 
   //inserindo o texto na tela
   return(
     <View>
-      <Text>FORMULÁRIO DOS CRIAS</Text>
+      <Text>FORMULÁRIO</Text>
 
       <TextInput
           placeholder="Digite o Seu Nome"//mensagem
           value={nome}//inserir o valor nome
-          onChangeText={setNome} //jogando o valor Nome vai ser carregado no formulario
+          onChangeText={setNome} //jogando o valor NOME vai ser carregado no formulario
+      />
+
+<TextInput
+          placeholder="Digite O Seu Cargo"
+          value={cargo}
+          onChangeText={setCargo} //jogando o valor CARGO vai ser carregado no formulario
       />
 
       <TextInput
           placeholder="Digite a Sua Idade"
           value={idade}
-          onChangeText={setIdade} //jogando o valor Nome vai ser carregado no formulario
+          onChangeText={setIdade} //jogando o valor IDADE vai ser carregado no formulario
       />
 
-      <Button title="Mostrar Resultado" onPress= {mostrarResultado} ></Button>
+<TextInput
+          placeholder="Digite o Seu Telefone"
+          value={telefone}
+          onChangeText={setTelefone} //jogando o valor TELEFONE vai ser carregado no formulario
+      />
+
+<TextInput
+          placeholder="Digite o Seu Email"
+          value={email}
+          onChangeText={setEmail} //jogando o valor EMAIL vai ser carregado no formulario
+      />
+
+<TextInput
+          placeholder="Digite a Sua Data de Admissão"
+          value={da}
+          onChangeText={setDA} //jogando o valor DATA DE ADMISSÃO vai ser carregado no formulario
+      />
+
+      <Button title="Cadastar" onPress= {mostrarResultado} ></Button>
 
 
       {resultado?(
